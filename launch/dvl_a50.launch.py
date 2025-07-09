d1330336c256ec54e26c1a8bbc518530cb6768eb
xelisce <xelisce@gmail.com>
2025-07-09 22:34:32 +0800

    Update a50 launch file

--- launch/dvl_a50.launch.py
+++ launch/dvl_a50.launch.py
@@ -4,18 +4,14 @@ import launch.substitutions
 import launch_ros.actions
 
 def generate_launch_description():
-    launch.actions.DeclareLaunchArgument('ip_address', default_value='192.168.194.95'),
-    launch.actions.DeclareLaunchArgument('position_frame_id', default_value="map"),
-    launch.actions.DeclareLaunchArgument('velocity_frame_id', default_value="base_link"),
-    launch.actions.DeclareLaunchArgument('altitude_frame_id', default_value="pool_bottom"),
     dvl_a50 = launch_ros.actions.Node(
         package='dvl_a50', 
         executable='dvl_a50_sensor', 
         parameters=[
-            {'dvl_ip_address': launch.substitutions.LaunchConfiguration('ip_address')},
-            {'position_frame_id': launch.substitutions.LaunchConfiguration('position_frame_id')},
-            {'velocity_frame_id': launch.substitutions.LaunchConfiguration('velocity_frame_id')},
-            {'altitude_frame_id': launch.substitutions.LaunchConfiguration('altitude_frame_id')},
+            {'dvl_ip_address': "192.168.194.95"},
+            {'position_frame_id': "map"},
+            {'velocity_frame_id': "base_link"},
+            {'altitude_frame_id': "pool_bottom"},
         ],
         output='screen')
 
